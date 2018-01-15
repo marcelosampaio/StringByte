1f1388bad9ff64737e2043595847e33adce73d7f
Marcelo Sampaio <marcelo@artlogica.com.br>
2018-01-15 17:04:27 -0200

    Initial Commit

---
+++ StringByte/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StringByte
+//
+//  Created by Marcelo on 1/15/18.
+//  Copyright © 2018 Marcelo Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
